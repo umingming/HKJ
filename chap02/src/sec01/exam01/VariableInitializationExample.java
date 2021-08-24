af3b787f134c632715757ff6ffa290802ca3cfd0
umingming <u_0112@naver.com>
2021-08-24 16:42:00 +0900

    변수 선언과 저장

--- chap02/src/sec01/exam01/VariableInitializationExample.java
+++ chap02/src/sec01/exam01/VariableInitializationExample.java
@@ -3,7 +3,7 @@ package sec01.exam01;
 public class VariableInitializationExample {
 	public static void main(String[] args) {
 		
-		int value = 5;
+		int value = 8;
 		int result = value +10;
 		System.out.println(result);
 		
