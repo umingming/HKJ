34b04b54dafb29e62c29fd23ab7b4c420c34c71c
umingming <u_0112@naver.com>
2021-08-25 11:39:38 +0900

    정수타입

---
+++ chap02/src/sec02/exam02/ByteExample.java
@@ -0,0 +1,26 @@
+package sec02.exam02;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		byte var1 = -128;
+		byte var2 = -127;
+		byte var3 = 0;
+		byte var5 = 127;
+		
+		long balance1 = 300000;
+		long balance3 = 20000000000000l;
+		
+		char a1 = 'A';
+		int a2 = 'A';
+		
+		System.out.println(a1);
+		System.out.println(a2);
+				
+		
+		
+				
+		
+	}
+
+}
