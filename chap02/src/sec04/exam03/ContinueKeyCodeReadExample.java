44bd8fa9044417e13da5c1289e1232b2bb4c6833
umingming <u_0112@naver.com>
2021-08-25 19:34:54 +0900

    종료하기

---
+++ chap02/src/sec04/exam03/ContinueKeyCodeReadExample.java
@@ -0,0 +1,21 @@
+package sec04.exam03;
+
+public class ContinueKeyCodeReadExample {
+
+	public static void main(String[] args) throws Exception {
+		int keyCode;
+		
+		while(true) {
+			keyCode = System.in.read();
+			System.out.println(keyCode);
+			
+			if(keyCode == 113) {
+				break;
+			}
+			
+		}
+
+		System.out.println("종료합니다.");
+	}
+
+}
