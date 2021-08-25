b259d8c5affbf9693cbdcb55190d102447c18909
umingming <u_0112@naver.com>
2021-08-25 11:13:16 +0900

    오류

--- chap02/src/sec01/exam04/VariableScopeExample.java
+++ chap02/src/sec01/exam04/VariableScopeExample.java
@@ -3,12 +3,18 @@ package sec01.exam04;
 public class VariableScopeExample {
 
 	public static void main(String[] args) {
-		int v1 = 15;
-		if(v1>10) {
-			int v2;
-			v2 = v1 - 10;
+		int v1 = 0;
+		if(true) {
+			int v2 = 0;
+			if(true) {
+				int v3 = 0;
+				v1 = 1;
+				v2 = 1;
+				v3 = 1;
+			}
+			v1 = v2 + v3;
 		}
-		int v3 = v1 + v2 + 5;
+		System.out.println(v3);;
 	}
 
 }
