d30186af930a50928502accbdb176337ed4b12fb
umingming <u_0112@naver.com>
2021-08-25 15:09:50 +0900

    자동 타입 변환

---
+++ chap02/src/sec03/exam01/PromotionExample.java
@@ -0,0 +1,31 @@
+package sec03.exam01;
+
+public class PromotionExample {
+
+	public static void main(String[] args) {
+
+		byte byteValue = 10;
+		int intValue = byteValue;
+		System.out.println(intValue);
+		
+		char charValue = '°¡';
+		intValue = charValue;
+		System.out.println(intValue);
+		
+		intValue = 50;
+		long longValue = intValue;
+		System.out.println(longValue);
+		
+		longValue = 100;
+		float floatValue = longValue; 
+		System.out.println(floatValue);
+		
+		floatValue = 100.5f;
+	    double doubleValue = floatValue;
+		System.out.println(doubleValue);
+		
+		
+		
+	}
+
+}
