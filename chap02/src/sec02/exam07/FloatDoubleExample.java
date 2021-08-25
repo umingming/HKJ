ad6afd793f71f4bfa625ef59e8c9a987a7781fae
umingming <u_0112@naver.com>
2021-08-25 13:35:57 +0900

    실수 타입

---
+++ chap02/src/sec02/exam07/FloatDoubleExample.java
@@ -0,0 +1,29 @@
+package sec02.exam07;
+
+public class FloatDoubleExample {
+
+	public static void main(String[] args) {
+		
+		float var1 = 3.14f;
+		float var2 = 0.12345678901234567890f;
+		double var3 = 0.12345678901234567890;
+		
+		System.out.println("var2: " + var2);
+		System.out.println("var3: " + var3);
+		
+		double var4 = 3e6;
+		double var5 = 3e-3;
+		float var6 = 3e6f;
+		
+		System.out.println("var4: " + var4);
+		System.out.println("var5: " + var5);
+		System.out.println("var6: " + var6);
+		
+		
+		
+		
+	
+
+	}
+
+}
