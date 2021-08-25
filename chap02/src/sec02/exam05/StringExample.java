c6a4de51308292fc6bf69954e8e2eb62d507f4af
umingming <u_0112@naver.com>
2021-08-25 11:54:47 +0900

    이스케이프 문자

---
+++ chap02/src/sec02/exam05/StringExample.java
@@ -0,0 +1,24 @@
+package sec02.exam05;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		String name = "홍길동";
+		String job = "프로그래머";
+		System.out.println(name);
+		System.out.println(job);
+		
+		String str = "나는 \"자바\"를 좋아합니다.";
+		System.out.println(str);
+		
+		String str2 = "번호\t이름\t나이";
+		System.out.println(str2);
+		
+		String str3 = "홍길동\n감자바";
+		System.out.println(str3);
+		
+		
+
+	}
+
+}
