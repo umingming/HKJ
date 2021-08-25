677fc6bae9a96e905629604fc8f12531928261d2
umingming <u_0112@naver.com>
2021-08-25 13:59:36 +0900

    boolean

--- chap02/src/sec02/exam08/BooleanExample.java
+++ chap02/src/sec02/exam08/BooleanExample.java
@@ -3,12 +3,13 @@ package sec02.exam08;
 public class BooleanExample {
 
 	public static void main(String[] args) {
-		boolean stop = true;
+		boolean stop = false;
 		if(stop) {
-			System.out.println("중지합니다.");
+			System.out.println("멈춥니다.");
 		}else {
-			System.out.println("시작합니다.");
+			System.out.println("출발합니다.");
 		}
+				
 		
 
 	}
