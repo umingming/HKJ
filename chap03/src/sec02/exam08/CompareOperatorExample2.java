3c913715ff458b3742e6d488fbf1f3f3ccc24839
umingming <u_0112@naver.com>
2021-08-26 21:13:32 +0900

    비교2

---
+++ chap03/src/sec02/exam08/CompareOperatorExample2.java
@@ -0,0 +1,17 @@
+package sec02.exam08;
+
+public class CompareOperatorExample2 {
+
+	public static void main(String[] args) {
+		int v2 = 1;
+		double v3 = 1.0;
+		System.out.println(v2==v3); //true
+		
+		double v4 = 0.1;
+		double v5 = 0.1f;
+		System.out.println(v4 == v5); //false
+		System.out.println((float)v4 == v5); //true
+
+	}
+
+}
