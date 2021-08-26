e5f1339b1e38669bd49b6e2ded62e5582cf39b9d
umingming <u_0112@naver.com>
2021-08-26 21:28:03 +0900

    대입 연산자

---
+++ chap03/src/sec02/exam10/AssignmentOperatorExample.java
@@ -0,0 +1,20 @@
+package sec02.exam10;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		int result = 1;
+		result += 9;
+		System.out.println(result); //10
+		result -= 5;
+		System.out.println(result); //5
+		result *= 3;
+		System.out.println(result); //15
+		result /= 5;
+		System.out.println(result); //3
+		result %= 3;
+		System.out.println(result); //0
+
+	}
+
+}
