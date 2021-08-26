279ddb25a113930b728ef6eb71e3aef756c6ebb0
umingming <u_0112@naver.com>
2021-08-26 20:26:25 +0900

    연산자

---
+++ chap03/src/sec02/exam03/DenyLogicOperatorExample.java
@@ -0,0 +1,20 @@
+package sec02.exam03;
+
+public class DenyLogicOperatorExample {
+
+	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play; //false
+		System.out.println(play); //false
+
+		play = !play;
+		System.out.println(play); //true
+		
+		
+		
+
+	}
+
+}
