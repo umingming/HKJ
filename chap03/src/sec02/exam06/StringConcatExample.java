be417b0acacb609a0afaeca2fd133690e7afbb10
umingming <u_0112@naver.com>
2021-08-26 20:52:49 +0900

    string+

---
+++ chap03/src/sec02/exam06/StringConcatExample.java
@@ -0,0 +1,14 @@
+package sec02.exam06;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+		String str1 = "JDK" + 3 +3.0;   //JDK3.30
+		String str2 = 3 + 3.0 + "JDK";  //6.0JDK
+		System.out.println(str1);
+		System.out.println(str2);
+		
+
+	}
+
+}
