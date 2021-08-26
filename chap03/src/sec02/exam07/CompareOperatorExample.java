b11145f097a444c8387faf07ac36a7dae76dde16
umingming <u_0112@naver.com>
2021-08-26 20:57:05 +0900

    비교 연산자

---
+++ chap03/src/sec02/exam07/CompareOperatorExample.java
@@ -0,0 +1,24 @@
+package sec02.exam07;
+
+public class CompareOperatorExample {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 10;
+		boolean result1 = (num1 == num2);
+		boolean result2 = (num1 != num2);
+		boolean result3 = (num1 <= num2);
+		System.out.println(result1); //true
+		System.out.println(result2); //false
+		System.out.println(result3); //true
+		
+		char char1 = 'A';
+		char char2 = 'B';
+		boolean result4 = (char1 < char2);
+		System.out.println(result4); //true
+		
+		
+
+	}
+
+}
