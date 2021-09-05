0b2852b73fd7fbce63fe5dca9fd6f41fbddc6756
umingming <u_0112@naver.com>
2021-09-06 00:04:00 +0900

    점수 변경

--- chap04/src/sec01/exam03/IfElseIfElseExample.java
+++ chap04/src/sec01/exam03/IfElseIfElseExample.java
@@ -3,11 +3,12 @@ package sec01.exam03;
 public class IfElseIfElseExample {
 
 	public static void main(String[] args) {
-		int score = 35;
+		int score = 37;
 		
 		if(score>=90) {
 			System.out.println("점수가 90보다 큽니다.");
 			System.out.println("등급은 A입니다.");
+			
 		} else if(score>=80){
 			System.out.println("점수가 90보다 작습니다.");
 			System.out.println("등급은 B입니다.");
