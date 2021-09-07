c5d1e9765c699d70fa7541426ef7231f9836499b
umingming <u_0112@naver.com>
2021-09-07 13:12:50 +0900

    초기화식 생략

---
+++ chap04/src/sec02/exam01/ForPrintFrom1To10Example.java
@@ -0,0 +1,13 @@
+package sec02.exam01;
+
+public class ForPrintFrom1To10Example {
+
+	public static void main(String[] args) {
+		int i=1;
+		for(; i<=10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
