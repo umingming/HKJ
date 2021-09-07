97699fdac65b047bfbed15a2d45a16609a14c731
umingming <u_0112@naver.com>
2021-09-07 13:36:25 +0900

    while 출력

--- chap04/src/sec02/exam06/WhilePrintFom1To10Example.java
+++ chap04/src/sec02/exam06/WhilePrintFom1To10Example.java
@@ -3,7 +3,11 @@ package sec02.exam06;
 public class WhilePrintFom1To10Example {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i = 1;
+		while (i<=10) {
+			System.out.println(i);
+			i++;
+		}
 
 	}
 
