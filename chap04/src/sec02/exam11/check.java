296b1fc2c73ac1ed2f2712a712f2ead66f3f41e3
umingming <u_0112@naver.com>
2021-09-07 21:31:41 +0900

    04-2)4

--- chap04/src/sec02/exam11/check.java
+++ chap04/src/sec02/exam11/check.java
@@ -4,12 +4,11 @@ public class check {
 
 	public static void main(String[] args) {
 		
-		while(true) {
-			int num1 = (int)(Math.random() * 6) + 1;
-			int num2 = (int)(Math.random() * 6) + 1;
-			System.out.println("(" + num1 + ", " + num2 + ")");
-			if (num1+num2==5) {
-				break;
+		for(int x=1; x<=10; x++) {
+			 for(int y=1; y<=10; y++) {
+			  if(4*x+5*y==60) {
+				  System.out.println("(" + x + ", " + y + ")");
+			  }
 			}
 		}
 		
