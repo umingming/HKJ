ec3cf51a6d436c5efdb867585fdbf964b33518bf
umingming <u_0112@naver.com>
2021-09-10 14:43:12 +0900

    5

--- chap05/src/sec02/Check.java
+++ chap05/src/sec02/Check.java
@@ -3,19 +3,23 @@ package sec02;
 public class Check {
 
 	public static void main(String[] args) {
-		int max = 0;
-		int[] array = { 1, 5, 3, 8, 2 };
+		
+		int[][] array = { {95, 86}, {83, 92, 96}, {78, 83, 93, 87, 88} };
 
-		//for문
-		int std = max;
-		for( int i=0; i<4; i++ ){
-		 std = i;
-		 if(std > max){     
-		  max = std;
+		int sum = 0;
+		double avg = 0.0;
+		
+		for(int i=0; i<array.length; i++) {
+			for(int k=0; k<array[i].length; k++) {
+				sum += array[i][k];
+			}
 		}
-		}  
-		System.out.println("max: " + max);
-
+		avg = (double)sum/(10);
+		
+		System.out.println("sum: " + sum);
+		System.out.println("avg: " + avg);
+		
+	
 	}
 
 
