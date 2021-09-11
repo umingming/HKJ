ef242b4e746947fc1d8f664b3e6b851bb1f0057c
umingming <u_0112@naver.com>
2021-09-11 19:26:27 +0900

    예제

--- chap05/src/sec03/Week.java
+++ chap05/src/sec03/Week.java
@@ -1,5 +1,14 @@
 package sec03;
 
 public enum Week {
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,                   
+	FRIDAY,
+	SATURDAY,
+	SUNDAY,
+} 
 
-}
+
+
---
+++ chap05/src/sec03/exam02/EnumWeekExample.java
@@ -0,0 +1,53 @@
+package sec03.exam02;
+
+import java.util.Calendar;
+
+import sec03.Week;
+
+public class EnumWeekExample {
+
+	public static void main(String[] args) {
+		
+		Week today = null;
+		
+		System.out.print("'");
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH);
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		int week = cal.get(Calendar.DAY_OF_WEEK);
+		int hour = cal.get(Calendar.HOUR);
+		int minute = cal.get(Calendar.MINUTE);
+		int second = cal.get(Calendar.SECOND);
+		
+
+
+		System.out.printf("age:%d%n", 27);
+		
+		switch(week) {
+		case 1:
+			today = Week.SUNDAY; break;
+		case 2:
+			today = Week.MONDAY; break;
+		case 3:
+			today = Week.TUESDAY; break;
+		case 4:
+			today = Week.WEDNESDAY; break;
+		case 5:
+			today = Week.THURSDAY; break;
+		case 6:
+			today = Week.FRIDAY; break;
+		case 7:
+			today = Week.SATURDAY; break;
+		}
+		
+		System.out.println("오늘 요일: " + today);
+		if(today == Week.SUNDAY) {
+			System.out.println("일요일에는 축구를 합니다.");
+		} else {
+			System.out.println("열심히 자바 공부합니다.");
+		}
+
+	}
+
+}
