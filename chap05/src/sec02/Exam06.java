3c03986f1412d5a003c735651b22799ae98f91fd
umingming <u_0112@naver.com>
2021-09-15 09:08:58 +0900

    객체

--- chap05/src/sec02/Exam06.java
+++ chap05/src/sec02/Exam06.java
@@ -10,49 +10,60 @@ public class Exam06 {
 		int studentNum = 0;
 		int[] scores = null;
 		Scanner scanner = new Scanner(System.in);
-		
+
 		while(run){
-			System.out.println("-----------------------------------------------------------");
-			System.out.println("1. 학생수 | 2. 점수입력 | 3. 점수리스트 | 4. 분석 | 5. 종료");
-			System.out.println("-----------------------------------------------------------");
-			System.out.print("선택> ");
-			 
-			int selectNo = Integer.parseInt(scanner.nextLine());
+		 System.out.println("-----------------------------------------------------------");
+		 System.out.println("1. 학생수 | 2. 점수입력 | 3. 점수리스트 | 4. 분석 | 5. 종료");
+		 System.out.println("-----------------------------------------------------------");
+		 System.out.print("선택> ");
 
-			if(selectNo == 1) {
-				System.out.print("학생 수> ");
-				studentNum = Integer.parseInt(scanner.nextLine());
-				scores = new int[studentNum];
-				
-			} else if (selectNo == 2) {
-				for (int i = 0; i < scores.length; i++) {
-					System.out.printf("scores[%d]> ", i);
-					scores[i] = Integer.parseInt(scanner.nextLine());
-				}
-			} else if (selectNo == 3) {
-				for (int i = 0; i < scores.length; i++) {
-					System.out.printf("scores[%d]: %d\n", i, scores[i]);
-				}
-			} else if (selectNo == 4) {
-				int max = 0;
-				int sum = 0;
-				double avg = 0.0;
-				for( int i = 0; i <scores.length; i++) {
-					max = (max < scores[i]) ? scores[i] : max;
-					sum += scores[i];
-				}
-				avg = (double)sum/studentNum;
-				System.out.printf("최고 점수: %d\n", max);
-				System.out.printf("평균 점수: %f\n", avg);
-				
-				
-			}else if(selectNo == 5) {
-				run = false;
-			}
+		 int selectNo = Integer.parseInt(scanner.nextLine());
 
-			 
+		 if(selectNo == 1) {
+		  System.out.print("학생수>");
+		  studentNum = Integer.parseInt(scanner.nextLine());
+		  System.out.println();
+		 
+		 } else if(selectNo == 2) {
+		  scores = new int[studentNum];
+		  System.out.print("scores[0]>");
+		  scores[0] = Integer.parseInt(scanner.nextLine());
+		  System.out.print("scores[1]>");
+		  scores[1] = Integer.parseInt(scanner.nextLine());
+		  System.out.print("scores[2]>");
+		  scores[2] = Integer.parseInt(scanner.nextLine());
 
+		 }else if(selectNo == 3) {
+		  System.out.println("scores[0]>" + scores[0]);
+		  System.out.println("scores[1]>" + scores[1]);
+		  System.out.println("scores[2]>" + scores[2]);
+
+		 }else if(selectNo == 4) {
+		  int maxScore;
+		  if(scores[0]>scores[1]){
+		   if(scores[0]>scores[2]){
+		    maxScore = scores[0];
+		   } else { 
+		    maxScore = scores[2];
+		   }
+		  }else {
+		   if(scores[1]<scores[2]){
+		    maxScore = scores[2];
+		   }else{
+		    maxScore = scores[1];
+		  } 
+		  int sum = 0;
+		  for( int score : scores ) {
+		   sum += score;
+		  }
+		  double avgScore = (double)sum / scores.length; 
+		  System.out.print("최고 점수: " + maxScore );
+		  System.out.print("평균 점수: " + avgScore );
+
+		 }}
+		  else  if(selectNo == 5) {
+		  System.out.print("프로그램 종료");
+		 }
 		}
-		System.out.println("프로그램 종료");
 	}
 }
