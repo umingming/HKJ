9f168e8fd479e099f776d228eed4d14f63824643
umingming <u_0112@naver.com>
2021-09-17 01:33:17 +0900

    오버로딩

--- chap06/src/sec03/exam02/CarExample.java
+++ chap06/src/sec03/exam02/CarExample.java
@@ -3,7 +3,6 @@ package sec03.exam02;
 public class CarExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Car car1 = new Car();
 		System.out.println("car1.company : " + car1.company);
 		System.out.println();
@@ -11,6 +10,7 @@ public class CarExample {
 		Car car2 = new Car("자가용");
 		System.out.println("car2.company : " + car2.company);
 		System.out.println("car2.model : " + car2.model);
+		System.out.println("car2.color : " + car2.color);
 		System.out.println();
 
 		Car car3 = new Car("자가용", "빨강");
