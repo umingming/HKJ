50e168292bad95c62d9cdd746ee023c447559692
umingming <u_0112@naver.com>
2021-09-19 00:08:30 +0900

    final

---
+++ chap06/src/sec05/exam05/Person.java
@@ -0,0 +1,5 @@
+package sec05.exam05;
+
+public class Person {
+
+}
---
+++ chap06/src/sec05/exam05/PersonExample.java
@@ -0,0 +1,10 @@
+package sec05.exam05;
+
+public class PersonExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
