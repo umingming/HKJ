9b1ed81e886d68a7be6c33aa47992138db1e62b6
umingming <u_0112@naver.com>
2021-10-15 00:45:15 +0900

    부모 클래스

---
+++ HKJ/src/chap07/sec02/exam05/Parent.java
@@ -0,0 +1,14 @@
+package chap07.sec02.exam05;
+
+public class Parent {
+	public String field1;
+	
+	public void methid1() {
+		System.out.println("Parent-method1()");
+	}
+
+	public void methid2() {
+		System.out.println("Parent-method2()");
+	}
+
+}
