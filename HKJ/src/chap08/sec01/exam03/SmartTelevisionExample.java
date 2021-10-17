2849eb6ed5f256e487b812f9c583109cbf4a26ab
umingming <u_0112@naver.com>
2021-10-17 16:20:24 +0900

    creat object using the implement class

---
+++ HKJ/src/chap08/sec01/exam03/SmartTelevisionExample.java
@@ -0,0 +1,12 @@
+package chap08.sec01.exam03;
+
+public class SmartTelevisionExample {
+
+	public static void main(String[] args) {
+		SmartTelevision tv = new SmartTelevision();
+		RemoteControl rc = tv;
+		Searchable searchavle = tv;
+
+	}
+
+}
