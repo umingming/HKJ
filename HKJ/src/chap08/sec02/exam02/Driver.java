669f3aa3a2e8079d0f96de165ffa665df5b4d521
umingming <u_0112@naver.com>
2021-10-17 17:08:58 +0900

    create a new interface

---
+++ HKJ/src/chap08/sec02/exam02/Driver.java
@@ -0,0 +1,8 @@
+package chap08.sec02.exam02;
+
+public class Driver {
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+
+}
---
+++ HKJ/src/chap08/sec02/exam02/Vehicle.java
@@ -0,0 +1,5 @@
+package chap08.sec02.exam02;
+
+public interface Vehicle {
+	public void run();
+}
