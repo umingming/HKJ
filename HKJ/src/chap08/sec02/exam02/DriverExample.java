e43a1621de293912acad33ad5c4a2167b30b5251
umingming <u_0112@naver.com>
2021-10-17 17:11:46 +0900

    test

---
+++ HKJ/src/chap08/sec02/exam02/DriverExample.java
@@ -0,0 +1,16 @@
+package chap08.sec02.exam02;
+
+public class DriverExample {
+
+	public static void main(String[] args) {
+		Driver driver = new Driver();
+		
+		Bus bus = new Bus();
+		Taxi taxi = new Taxi();
+		
+		driver.drive(bus);
+		driver.drive(taxi);
+
+	}
+
+}
