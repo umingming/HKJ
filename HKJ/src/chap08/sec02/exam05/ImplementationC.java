79a0b8e93c58e1ef2af883a1bb45c9f9add2d5a1
umingming <u_0112@naver.com>
2021-10-17 17:55:38 +0900

    this is a class implementing the interfaceC

---
+++ HKJ/src/chap08/sec02/exam05/ImplementationC.java
@@ -0,0 +1,24 @@
+package chap08.sec02.exam05;
+
+public class ImplementationC implements InterfaceC{
+
+	@Override
+	public void methodA() {
+		System.out.println("A실행");
+		
+	}
+
+	@Override
+	public void methodB() {
+		System.out.println("B실행");
+		
+	}
+
+	@Override
+	public void mehtodC() {
+		System.out.println("C실행");
+		
+	}
+
+
+}
