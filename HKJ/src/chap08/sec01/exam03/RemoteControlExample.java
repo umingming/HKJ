c1d8c9f79a8155d9f12c3a93c7735835ab148681
umingming <u_0112@naver.com>
2021-10-17 16:09:04 +0900

    in order to use interface variables, substitute the implementation object.

---
+++ HKJ/src/chap08/sec01/exam03/RemoteControlExample.java
@@ -0,0 +1,11 @@
+package chap08.sec01.exam03;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		RemoteControl rc = new Television();
+		rc.setVolume(39);
+
+	}
+
+}
