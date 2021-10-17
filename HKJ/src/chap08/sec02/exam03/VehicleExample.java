42eea07742fb814679f475dad1a80b8015d69bf5
umingming <u_0112@naver.com>
2021-10-17 17:35:47 +0900

    force type conversion

---
+++ HKJ/src/chap08/sec02/exam03/VehicleExample.java
@@ -0,0 +1,17 @@
+package chap08.sec02.exam03;
+
+public class VehicleExample {
+
+	public static void main(String[] args) {
+		Vehicle vehicle = new Bus();
+		
+		vehicle.run();
+		
+		Bus bus = (Bus) vehicle;
+		
+		bus.run();
+		bus.checkFare();
+
+	}
+
+}
