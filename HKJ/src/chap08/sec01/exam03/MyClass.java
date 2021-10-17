4974fbd2190b7dfe376e9827f3aa781fd7f6802a
umingming <u_0112@naver.com>
2021-10-17 16:31:14 +0900

    using a inteface

--- HKJ/src/chap08/sec01/exam03/MyClass.java
+++ HKJ/src/chap08/sec01/exam03/MyClass.java
@@ -3,15 +3,25 @@ package chap08.sec01.exam03;
 public class MyClass {
 	
 	RemoteControl rc = new Television();
+	
+	public MyClass() {
+	}
 
 	public MyClass(RemoteControl rc) {
 		this.rc = rc;
+		rc.turn0n();
+		rc.setVolume(5);
 	}
 	
 	void methodA() {
 		RemoteControl rc = new Audio();
+		rc.turn0n();
+		rc.setVolume(5);
 	}
 	
-	void methodB(RemoteControl rc) { }
+	void methodB(RemoteControl rc) { 
+		rc.turn0n();
+		rc.setVolume(5);
+	}
 
 }
