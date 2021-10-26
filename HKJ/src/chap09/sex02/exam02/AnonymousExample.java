24933acc0da2aa59970c2b48a4820842bb1a57c0
umingming <u_0112@naver.com>
2021-10-26 21:38:41 +0900

    Anonymous implemeting obj

---
+++ HKJ/src/chap09/sex02/exam02/AnonymousExample.java
@@ -0,0 +1,29 @@
+package chap09.sex02.exam02;
+
+public class AnonymousExample {
+
+	public static void main(String[] args) {
+		Anonymous anony = new Anonymous();
+		anony.field.turnOn();
+		anony.method1();
+		anony.method2(
+				new RemoteControl() {
+					
+					@Override
+					public void turnOn() {
+						System.out.println("컴퓨터를 켭니다.");
+						
+					}
+					
+					@Override
+					public void turnOff() {
+						System.out.println("컴퓨터를 끕니다.");
+						
+					}
+				}
+				);
+		
+
+	}
+
+}
---
+++ HKJ/src/chap09/sex02/exam02/RemoteControl.java
@@ -0,0 +1,7 @@
+package chap09.sex02.exam02;
+
+public interface RemoteControl {
+	public void turnOn();
+	public void turnOff();
+}
+
