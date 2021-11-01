a89f19c54c9ebb12838119079e496a08d1239ef0
umingming <u_0112@naver.com>
2021-11-02 00:56:23 +0900

    multi catch

---
+++ HKJ/src/chap10/sec02/exam03/CatchOrderExample.java
@@ -0,0 +1,22 @@
+package chap10.sec02.exam03;
+
+public class CatchOrderExample {
+
+	public static void main(String[] args) {
+		try {
+			String data1 = args[0];
+			String data2 = args[1];
+			int value1 = Integer.parseInt(data1);
+			int value2 = Integer.parseInt(data2);
+			int result = value1 + value2;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("실행 매개값의 수가 부족합니다.");
+		} catch(Exception e) {
+			System.out.println("실행에 문제가 있습니다.");
+		} finally {
+			System.out.println("다시 실행하세요.");
+		}
+
+	}
+
+}
