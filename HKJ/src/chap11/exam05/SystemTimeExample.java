6b21c2b0b8297236f8b9fa17fa3fa8ec2fe2a87b
umingming <u_0112@naver.com>
2021-11-03 00:22:25 +0900

    get Time

---
+++ HKJ/src/chap11/exam05/SystemTimeExample.java
@@ -0,0 +1,20 @@
+package chap11.exam05;
+
+public class SystemTimeExample {
+
+	public static void main(String[] args) {
+		long time1 = System.nanoTime();
+		
+		int sum = 0;
+		for(int i=0; i<=1000000; i++) {
+			sum += i;
+		}
+		
+		long time2 = System.nanoTime();
+		
+		System.out.println(sum);
+		System.out.println(time2 - time1);
+
+	}
+
+}
