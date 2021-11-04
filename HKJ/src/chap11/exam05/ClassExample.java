ce5cfb585ba619d97d09989182b602aac8b8bf0a
umingming <u_0112@naver.com>
2021-11-04 11:21:12 +0900

    Check

--- HKJ/src/chap11/exam05/ClassExample.java
+++ HKJ/src/chap11/exam05/ClassExample.java
@@ -5,16 +5,16 @@ import chap08.sec02.exam01.Car;
 public class ClassExample {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		Class c = Car.class;
-//		Class c2 = Class.forName("sec02.exam01.Car");
-//		Car car = new Car();
-//		Class c3 = car.getClass();
-		
-		
-		System.out.println(c.getName());
-		System.out.println(c.getSimpleName());
-		System.out.println(c.getPackage().getName());
-
+//		Class c = Car.class;
+////		Class c2 = Class.forName("sec02.exam01.Car");
+////		Car car = new Car();
+////		Class c3 = car.getClass();
+//		
+//		
+//		System.out.println(c.getName());
+//		System.out.println(c.getSimpleName());
+//		System.out.println(c.getPackage().getName());
+		System.out.println("안녕하세요\r 저는");
 	}
 
 }
